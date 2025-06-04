f5cc60b1f25cbd1272b6d5411290e7b31e68b6ca
minoti <minotimondal1010@gmail.com>
2025-06-04 15:33:08 +0530

    hello

---
+++ lib/view/view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class view extends StatelessWidget{
+
+  @override
+  Widget build (BuildContext context){
+    return Scaffold(
+       body: Text("hello"),
+    );
+  }
+}
